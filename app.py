ed8c2f6a668b1890bb80ba80cc2b654e8d6f1254
Roy Sam <roy.sam@live.com>
2016-01-26 15:49:24 +0800

    test

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#Enable automatic config changes reload
+app.config["DEBUG"] = True
+
 #decorator  - route URL "/" to function super_world()
  
 @app.route("/")
@@ -14,7 +17,36 @@ def super_world():
 @app.route("/hello")
 
 def hello_world():
-    return "Hello,Junos Automation"
+    return "Hello world,Junos Automation"
+
+#Dynamic Route
+@app.route("/test/<search_query>")
+
+def search(search_query):
+    message = "This is the input: " + search_query    
+    return message
+
+@app.route("/integer/<int:value>")
+
+def int_type(value):
+    value += 1
+    return "Incremented: " + str(value)
+
+@app.route("/directory/<path:value>")
+
+def vm_directory(value):
+    return "Repository: " + value
+
+@app.route("/name/<name>")
+
+def find_contact(name):
+    if name.lower() == "juniper":
+        return "Hello Juniper Networks"
+    else:
+        return "Unknown Tech Company", 404
+
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
